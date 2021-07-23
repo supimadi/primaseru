24ee90c229acb40f0703bcb5fd9c7a258e536e52
Lukman Hakim <hlukman156@gmail.com>
2021-07-23 13:22:07 +0700

    a new homepage

--- homepage/static/homepage/js/home.js
+++ homepage/static/homepage/js/home.js
@@ -1,39 +1,15 @@
-$(document).ready(() => {
-    $('.carousel').slick({
-        dots: false,
-        arrows: false,
-        infinite: true,
-        respondTo: 'min',
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 441,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    autoplay: true,
-                    autoplaySpeed: 1800,
-                }
-            }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
-        ]
-    });
+const navbar = document.querySelector('.navbar');
+const navToggler = document.querySelector('.navbar-toggler');
 
+document.addEventListener('scroll', ()=> {
+    const scrollTop = this.pageYOffset;
+    if(scrollTop >= 50){
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
 });
+
+navToggler.addEventListener('click', ()=> {
+    navToggler.classList.toggle('active')
+})
