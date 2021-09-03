882519cee96fb2b4e504d7449ba02f0e15176df0
Lukman Hakim <hlukman156@gmail.com>
2021-09-03 08:41:02 +0700

    update help text

--- homepage/forms.py
+++ homepage/forms.py
@@ -36,7 +36,7 @@ class ParticipantRegisterForm(forms.Form):
     parent_phone_number = forms.CharField(label='No. HP Orang Tua/Wali')
     homeroom_teacher_phone_number = forms.CharField(label='No. HP Wali Kelas', help_text='Isi dengan nomor HP wali kelas, kelas 9 di SMP.')
     bk_teacher_phone_number = forms.CharField(label='No. HP Guru BK', help_text='Isi dengan nomor HP BK, kelas 9 di SMP.', required=False)
-    info = forms.ModelMultipleChoiceField(label='Info Primaseru (PPDB)', queryset=InfoSourcePPDB.objects.all(), help_text="Pilih dari mana Anda pengetahui Primaseru.")
+    info = forms.ModelMultipleChoiceField(label='Info Primaseru (PPDB)', queryset=InfoSourcePPDB.objects.all(), help_text="Pilih dari mana Anda pengetahui Primaseru (boleh lebih dari 1 pilihan).")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
