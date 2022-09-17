a027aa09ef86aaebc9f2bf67d41d2e5be141b18a
Lukman Hakim <hlukman156@gmail.com>
2022-09-17 10:14:32 +0700

    rework countdown timer

--- homepage/static/homepage/js/homepage.js
+++ homepage/static/homepage/js/homepage.js
@@ -133,26 +133,25 @@ function update_chart_onresize(config){
     handle_device_change(smallDevice);
 }
 
-function update_countdown() {
+function update_countdown(today, day, hour, minute, second, container) {
     setInterval(() => {
         let now = new Date().getTime();
-        let distance = date - now;
+        let distance = today - now;
 
         let days = Math.floor(distance / (1000 * 60 * 60 * 24));
         let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        document.getElementById('day').innerHTML = days;
-        document.getElementById('hour').innerHTML = hours;
-        document.getElementById('minute').innerHTML = minutes;
-        document.getElementById('second').innerHTML = seconds;
+        document.getElementById(day).innerHTML = days;
+        document.getElementById(hour).innerHTML = hours;
+        document.getElementById(minute).innerHTML = minutes;
+        document.getElementById(second).innerHTML = seconds;
 
         // If the count down is finished, write some text
         if (distance <= 0) {
             clearInterval(update_countdown);
-            document.getElementById("countdown").innerHTML = "Primaseru Telah Ditutup.";
-            document.getElementById("schedule-headline").innerHTML = "Sampai Jumpa Dilain Waktu.";
+            document.getElementById(container).innerHTML = "Jalur Pendaftar Telah Ditutup.";
         }
 
     }, 1000)
